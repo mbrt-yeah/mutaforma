7cd4c027417fe2e5cc9db2e29da2cbd35fdfd0b4
mbrt-yeah <contact@mbrt.io>
2024-09-12 13:40:37 +0200

    Adds page footer and github logos

--- packages/docs/src/AppShell.vue
+++ packages/docs/src/AppShell.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+    import PageFooter from "@/components/PageFooter.vue";
     import PageHeader from "@/components/PageHeader.vue";
 </script>
 
 <template>
     <div className="page-wrapper">
-        <div className="sidebar">
-            <PageHeader />
-            <slot name="navigation" />
+        <div className="sidebar-wrapper">
+            <div className="sidebar">
+                <PageHeader />
+                <slot name="navigation" />
+                <PageFooter />
+            </div>
         </div>
         <main className="main-content">
             <slot name="main" />
